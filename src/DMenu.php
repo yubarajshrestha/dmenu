3d9dee74d045493894c7857a9120ec40330c9a9b
Yubaraj Shrestha <companion.krish@outlook.com>
2019-11-13 22:19:24 +0545

    Routes Updated

--- src/DMenu.php
+++ src/DMenu.php
@@ -1,7 +1,71 @@
 <?php
 namespace YubarajShrestha\DMenu;
 
-interface DMenu
+use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
+use YubarajShrestha\DMenu\Exceptions\DMenuException;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use YubarajShrestha\DMenu\Interfaces\DMenuInterface;
+
+class DMenu
 {
-    public function dMenu();
+
+	/** @var integer */
+	public $id;
+
+    /** @var string */
+    public $title;
+
+    /** @var string */
+    public $url;
+
+    /** @var string */
+    public $model;
+
+    /** @var \Illuminate\Support\Collection */
+    public $items;
+
+    public function __construct($resolver)
+    {
+        $this->id = Str::slug($resolver['name']);
+        $this->title = $resolver['name'];
+        $this->url = $resolver['url'];
+        $this->model = urlencode($resolver['model']);
+        $this->items = $this->resolveItems($resolver['model'] . '@getMenuItems');
+    }
+
+    protected function resolveItems($resolver): Collection
+    {
+        $resolver = Arr::wrap($resolver);
+        $items = app()->call(
+            array_shift($resolver), $resolver
+        );
+        return collect($items)->map(function ($dMenu) {
+            return $this->castToFeedItem($dMenu);
+        });
+    }
+
+    protected function castToFeedItem($dMenu): MenuItems
+    {
+        if (is_array($dMenu)) {
+            $dMenu = new MenuItems($dMenu);
+        }
+        if ($dMenu instanceof MenuItems) {
+            $dMenu->validate();
+        }
+        if (! $dMenu instanceof DMenuInterface) {
+            throw DMenuException::notFeedable($dMenu);
+        }
+
+        $menuItem = $dMenu->dMenu();
+
+        if (! $menuItem instanceof MenuItems) {
+            throw DMenuException::notAFeedItem($menuItem);
+        }
+        $menuItem->validate();
+        return $menuItem;
+    }
+
 }
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,6 +4,7 @@
 		$routeName = $data['route-name'];
 	}
     Route::get($routeName, 'YubarajShrestha\DMenu\Controllers\DMenuController@index')->name('dmenu.index');
+    Route::get($routeName . '/{model}/fetch', 'YubarajShrestha\DMenu\Controllers\DMenuController@fetch')->name('dmenu.fetch');
     Route::post($routeName, 'YubarajShrestha\DMenu\Controllers\DMenuController@store')->name('dmenu.store');
     Route::put($routeName . '/{id}', 'YubarajShrestha\DMenu\Controllers\DMenuController@update')->name('dmenu.update');
     Route::delete($routeName . '/{id}', 'YubarajShrestha\DMenu\Controllers\DMenuController@delete')->name('dmenu.delete');
